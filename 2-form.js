62e919aec6fc6c9003af50480a53afa72e8e7885
strelnikovartem <piramida1237@gmail.com>
2025-08-16 21:05:45 +0000

    Deploying to gh-pages from @ strelnikovartem/goit-jjs-hw-09@d15121343c64caaee253ec79d2502c2edcc2f07b 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */document.querySelector(".feedback-form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector(".feedback-form");t.addEventListener("input",n);function n(e){const o=e.target.value;console.dir(o)}
 //# sourceMappingURL=2-form.js.map
